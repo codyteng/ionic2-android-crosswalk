1539981be749abc8edd5618ed2d355a6dec2d5cc
Cody Teng <yuchuen90@hotmail.com>
2016-08-23 18:07:31 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Ionic2 - NavMenu Template
+
+Author: CodeDee
+
+## Getting started
+
+1. Install latest Ionic2
+    ```
+    npm install ionic@beta
+    ```
+
+2. Clone this repository
+    ```
+    git clone https://github.com/codyteng/ionic2-sample.git
+    ```
+    
+3. Change directory to ionic2-sample
+    ```
+    cd ionic2-sample
+    ```
+    
+4. Install dependencies
+    ```
+    npm install
+    ```
+    
+5. Start the app in browser.
+    ```
+    ionic serve
+    ```
+    
+7. To build your code into Native project, install Cordova
+    ```
+    npm install -g cordova
+    ```
+    
+8. Add platform to your code, iOS & Android.
+    ```
+    ionic platform add ios
+    ionic platform add android
+    ```
+    
+9. To run added platform on emulator / physical device. You should have emulator / physical device attached to your system.
+    ```
+    ionic run android
+    ionic run ios
+    ```
+
+You should be able to expand your awesome-app development from here.
+
---
+++ app/app.ts
@@ -0,0 +1,48 @@
+import {Component, ViewChild} from '@angular/core';
+import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+
+import { AboutPage } from './pages/about/about';
+import { SettingsPage } from './pages/settings/settings';
+
+@Component({
+  templateUrl: 'build/app.html'
+})
+class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  // make HelloIonicPage the root (or first) page
+  rootPage: any = AboutPage;
+  pages: Array<{title: string, component: any}>;
+
+  constructor(
+    public platform: Platform,
+    public menu: MenuController
+  ) {
+    this.initializeApp();
+
+    // set our app's pages
+    // side menu will show in this top-down order
+    this.pages = [
+      { title: 'About', component: AboutPage },
+      { title: 'Settings', component: SettingsPage}
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+}
+
+ionicBootstrap(MyApp);
